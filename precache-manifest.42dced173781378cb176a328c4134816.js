869febeb8735a49c9a9cdf9ce745b7441f2ba3e5
Cmoney <cascherer42@gmail.com>
2019-01-09 23:43:30 -0600

    Updates

---
+++ precache-manifest.42dced173781378cb176a328c4134816.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9013d5382170fd5663eb",
+    "url": "/static/js/main.9013d538.chunk.js"
+  },
+  {
+    "revision": "cf9ab8b4e0ddfb017e78",
+    "url": "/static/js/1.cf9ab8b4.chunk.js"
+  },
+  {
+    "revision": "9013d5382170fd5663eb",
+    "url": "/static/css/main.26e35579.chunk.css"
+  },
+  {
+    "revision": "c3ba03d821fd5bebec912c799076c9b6",
+    "url": "/index.html"
+  }
+];
